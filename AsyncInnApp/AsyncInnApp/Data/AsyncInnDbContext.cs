d49308f539f4299c9ddca871f2c881dd087b64a0
Clarice Costello <claricostello@outlook.com>
2019-01-25 13:44:43 -0800

    create Data folder and DbContext

---
+++ AsyncInnApp/AsyncInnApp/Data/AsyncInnDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInnApp.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
