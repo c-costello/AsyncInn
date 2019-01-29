29290947dcd4e2da7a1734a6d4079496f8e8a1df
Clarice Costello <claricostello@outlook.com>
2019-01-28 22:07:18 -0800

    seeded data

--- AsyncInnApp/AsyncInnApp/Migrations/20190129060449_seeddata.Designer.cs
+++ AsyncInnApp/AsyncInnApp/Migrations/20190129060449_seeddata.Designer.cs
@@ -4,14 +4,16 @@ using AsyncInnApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AsyncInnApp.Migrations
 {
     [DbContext(typeof(AsyncInnDbContext))]
-    partial class AsyncInnDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190129060449_seeddata")]
+    partial class seeddata
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
